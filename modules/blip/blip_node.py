5e1e4b3e3595537ac3c8aa6bf92f46980eff9aff
Tung Nguyen <tung.nguyen@sipher.xyz>
2023-08-25 04:32:00 +0700

    integrate blip

---
+++ modules/blip/blip_node.py
@@ -0,0 +1,192 @@
+import os
+
+import torch
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+
+import folder_paths
+from comfy.model_management import text_encoder_device, soft_empty_cache
+
+from ..model_downloader import load_models
+
+blip = None
+blip_size = 384
+blip_device = text_encoder_device()
+blip_current_device = None
+cpu = torch.device("cpu")
+model_url = "https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_caption_capfilt_large.pth"
+
+
+# Freeze PIP modules
+def packages(versions=False):
+    import subprocess
+    import sys
+
+    return [
+        (r.decode().split("==")[0] if not versions else r.decode())
+        for r in subprocess.check_output(
+            [sys.executable, "-m", "pip", "freeze"]
+        ).split()
+    ]
+
+
+# Tensor to PIL
+def tensor2pil(image):
+    return Image.fromarray(
+        np.clip(255.0 * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
+    )
+
+
+# Convert PIL to Tensor
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+
+
+def transformImage_legacy(input_image):
+    raw_image = input_image.convert("RGB")
+    raw_image = raw_image.resize((blip_size, blip_size))
+    transform = transforms.Compose(
+        [
+            transforms.Resize(raw_image.size, interpolation=InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.48145466, 0.4578275, 0.40821073),
+                (0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
+    image = transform(raw_image).unsqueeze(0).to(blip_device)
+    return image
+
+
+def transformImage(input_image):
+    raw_image = input_image.convert("RGB")
+    raw_image = raw_image.resize((blip_size, blip_size))
+    transform = transforms.Compose(
+        [
+            transforms.Resize(raw_image.size, interpolation=InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.48145466, 0.4578275, 0.40821073),
+                (0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
+    image = transform(raw_image).unsqueeze(0).to(blip_device)
+    return image.view(
+        1, -1, blip_size, blip_size
+    )  # Change the shape of the output tensor
+
+
+def load_blip():
+    global blip, blip_current_device
+    if blip is None:
+        blip_dir = os.path.join(folder_paths.models_dir, "blip")
+        if not os.path.exists(blip_dir):
+            os.mkdir(blip_dir)
+
+        files = load_models(
+            model_path=blip_dir,
+            model_url="https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_caption_capfilt_large.pth",
+            ext_filter=[".pth"],
+            download_name="model_base_caption_capfilt_large.pth",
+        )
+
+        from .models.blip import blip_decoder
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        med_config = os.path.join(current_dir, "configs", "med_config.json")
+        blip = blip_decoder(
+            pretrained=files[0],
+            image_size=blip_size,
+            vit="base",
+            med_config=med_config,
+        )
+        blip.eval()
+
+    if blip_current_device != blip_device:
+        blip_current_device = blip_device
+        blip = blip.to(blip_current_device)
+
+    return blip
+
+
+def unload_blip():
+    global blip, blip_current_device
+    if blip is not None:
+        blip_current_device = cpu
+        blip = blip.to(blip_current_device)
+
+    soft_empty_cache()
+
+
+class BlipCaption:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "min_length": (
+                    "INT",
+                    {
+                        "default": 24,
+                        "min": 0,  # minimum value
+                        "max": 200,  # maximum value
+                        "step": 1,  # slider's step
+                    },
+                ),
+                "max_length": (
+                    "INT",
+                    {
+                        "default": 48,
+                        "min": 0,  # minimum value
+                        "max": 200,  # maximum value
+                        "step": 1,  # slider's step
+                    },
+                ),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("caption",)
+
+    FUNCTION = "blip_caption"
+
+    CATEGORY = "Art Venture/Utils"
+
+    def blip_caption(self, image, min_length, max_length):
+        print(f"\033[34mStarting BLIP...\033[0m")
+
+        model = load_blip()
+        image = tensor2pil(image)
+
+        if "transformers==4.26.1" in packages(True):
+            print("Using Legacy `transformImaage()`")
+            tensor = transformImage_legacy(image)
+        else:
+            tensor = transformImage(image)
+
+        with torch.no_grad():
+            caption = model.generate(
+                tensor,
+                sample=False,
+                num_beams=1,
+                min_length=min_length,
+                max_length=max_length,
+            )
+
+        unload_blip()
+
+        return (caption[0],)
+
+
+NODE_CLASS_MAPPINGS = {"BLIPCaption": BlipCaption}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "BLIPCaption": "BLIP Caption",
+}
---
+++ modules/model_downloader.py
@@ -0,0 +1,101 @@
+import os
+import re
+from urllib.parse import urlparse
+
+
+def natural_sort_key(s, regex=re.compile("([0-9]+)")):
+    return [int(text) if text.isdigit() else text.lower() for text in regex.split(s)]
+
+
+def walk_files(path, allowed_extensions=None):
+    if not os.path.exists(path):
+        return
+
+    if allowed_extensions is not None:
+        allowed_extensions = set(allowed_extensions)
+
+    items = list(os.walk(path, followlinks=True))
+    items = sorted(items, key=lambda x: natural_sort_key(x[0]))
+
+    for root, _, files in items:
+        for filename in sorted(files, key=natural_sort_key):
+            if allowed_extensions is not None:
+                _, ext = os.path.splitext(filename)
+                if ext not in allowed_extensions:
+                    continue
+
+            # Skip hidden files
+            if "/." in root or "\\." in root:
+                continue
+
+            yield os.path.join(root, filename)
+
+
+def load_file_from_url(
+    url: str,
+    *,
+    model_dir: str,
+    progress: bool = True,
+    file_name: str | None = None,
+) -> str:
+    """Download a file from `url` into `model_dir`, using the file present if possible.
+
+    Returns the path to the downloaded file.
+    """
+    os.makedirs(model_dir, exist_ok=True)
+    if not file_name:
+        parts = urlparse(url)
+        file_name = os.path.basename(parts.path)
+    cached_file = os.path.abspath(os.path.join(model_dir, file_name))
+    if not os.path.exists(cached_file):
+        print(f'Downloading: "{url}" to {cached_file}\n')
+        from torch.hub import download_url_to_file
+
+        download_url_to_file(url, cached_file, progress=progress)
+    return cached_file
+
+
+def load_models(
+    model_path: str,
+    model_url: str = None,
+    ext_filter=None,
+    download_name=None,
+    ext_blacklist=None,
+) -> list:
+    """
+    A one-and done loader to try finding the desired models in specified directories.
+
+    @param download_name: Specify to download from model_url immediately.
+    @param model_url: If no other models are found, this will be downloaded on upscale.
+    @param model_path: The location to store/find models in.
+    @param ext_filter: An optional list of filename extensions to filter by
+    @return: A list of paths containing the desired model(s)
+    """
+    output = []
+
+    try:
+        for full_path in walk_files(model_path, allowed_extensions=ext_filter):
+            if os.path.islink(full_path) and not os.path.exists(full_path):
+                print(f"Skipping broken symlink: {full_path}")
+                continue
+            if ext_blacklist is not None and any(
+                full_path.endswith(x) for x in ext_blacklist
+            ):
+                continue
+            if full_path not in output:
+                output.append(full_path)
+
+        if model_url is not None and len(output) == 0:
+            if download_name is not None:
+                output.append(
+                    load_file_from_url(
+                        model_url, model_dir=model_path, file_name=download_name
+                    )
+                )
+            else:
+                output.append(model_url)
+
+    except Exception:
+        pass
+
+    return output
