16fb21d67bdd6883cf7be0e976aec812c568ed3e
Tung Nguyen <tung.nguyen@sipher.xyz>
2024-05-10 17:19:33 +0700

    add bedrock mistral llm node

--- modules/llm/chat.py
+++ modules/llm/chat.py
@@ -22,7 +22,8 @@ gpt_models = [
 
 gpt_vision_models = ["gpt-4-turbo", "gpt-4-turbo-preview", "gpt-4-vision-preview"]
 
-claude_models = ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"]
+claude3_models = ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"]
+claude2_models = ["claude-2.1"]
 
 aws_regions = [
     "us-east-1",
@@ -38,10 +39,21 @@ aws_regions = [
 
 bedrock_anthropic_versions = ["bedrock-2023-05-31"]
 
-bedrock_anthropic_models = [
+bedrock_claude3_models = [
     "anthropic.claude-3-haiku-20240307-v1:0",
     "anthropic.claude-3-sonnet-20240229-v1:0",
     "anthropic.claude-3-opus-20240229-v1:0",
+]
+
+bedrock_claude2_models = [
+    "anthropic.claude-v2",
+    "anthropic.claude-v2.1",
+]
+
+bedrock_mistral_models = [
+    "mistral.mistral-7b-instruct-v0:2",
+    "mistral.mixtral-8x7b-instruct-v0:1",
+    "mistral.mistral-large-2402-v1:0",
 ]
 
 default_system_prompt = "You are a useful AI agent."
@@ -98,7 +110,10 @@ class OpenAIApi(BaseModel):
     endpoint: Optional[str] = "https://api.openai.com/v1"
     timeout: Optional[int] = 60
 
-    def completion(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+    def chat(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+        if config.model not in gpt_models:
+            raise Exception(f"Must provide an OpenAI model, got {config.model}")
+
         formated_messages = [m.to_openai_message() for m in messages]
 
         url = f"{self.endpoint}/chat/completions"
@@ -117,9 +132,12 @@ class OpenAIApi(BaseModel):
         if data.get("error", None) is not None:
             raise Exception(data.get("error").get("message"))
 
-        content = data["choices"][0]["message"]["content"]
+        return data["choices"][0]["message"]["content"]
 
-        return content
+    def complete(self, prompt: str, config: LLMConfig, seed=None):
+        messages = [LLMMessage(role=LLMMessageRole.user, text=prompt)]
+
+        return self.chat(messages, config, seed)
 
 
 class ClaudeApi(BaseModel):
@@ -128,7 +146,10 @@ class ClaudeApi(BaseModel):
     version: Optional[str] = "2023-06-01"
     timeout: Optional[int] = 60
 
-    def completion(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+    def chat(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+        if config.model not in claude3_models:
+            raise Exception(f"Must provide a Claude v3 model, got {config.model}")
+
         system_message = [m for m in messages if m.role == "system"]
         user_messages = [m for m in messages if m.role != "system"]
         formated_messages = [m.to_claude_message() for m in user_messages]
@@ -149,9 +170,73 @@ class ClaudeApi(BaseModel):
         if data.get("error", None) is not None:
             raise Exception(data.get("error").get("message"))
 
-        content = data["content"][0]["text"]
+        return data["content"][0]["text"]
 
-        return content
+    def complete(self, prompt: str, config: LLMConfig, seed=None):
+        if config.model not in claude2_models:
+            raise Exception(f"Must provide a Claude v2 model, got {config.model}")
+
+        prompt = f"\n\nHuman: {prompt}\n\nAssistant:"
+        url = f"{self.endpoint}/complete"
+        data = {
+            "prompt": prompt,
+            "max_tokens_to_sample": config.max_token,
+            "temperature": config.temperature,
+        }
+        headers = {"x-api-key": self.api_key, "anthropic-version": self.version}
+
+        response = requests.post(url, json=data, headers=headers, timeout=self.timeout)
+        data: Dict = response.json()
+
+        if data.get("error", None) is not None:
+            raise Exception(data.get("error").get("message"))
+
+        return data["completion"]
+
+
+class AwsBedrockMistralApi(BaseModel):
+    aws_access_key_id: str
+    aws_secret_access_key: str
+    aws_session_token: Optional[str] = None
+    region: Optional[str] = aws_regions[0]
+    timeout: Optional[int] = 60
+    bedrock_runtime: Any = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        ensure_package("boto3", version="1.34.101")
+        import boto3
+
+        self.bedrock_runtime = boto3.client(
+            service_name="bedrock-runtime",
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            aws_session_token=self.aws_session_token,
+            region_name=self.region,
+        )
+
+    def chat(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+        raise Exception("Mistral doesn't support chat API")
+
+    def complete(self, prompt: str, config: LLMConfig, seed=None):
+        if config.model not in bedrock_mistral_models:
+            raise Exception(f"Must provide a Mistral model, got {config.model}")
+
+        prompt = f"<s>[INST]{prompt}[/INST]"
+        data = {
+            "prompt": prompt,
+            "max_tokens": config.max_token,
+            "temperature": config.temperature,
+        }
+
+        response = self.bedrock_runtime.invoke_model(body=json.dumps(data), modelId=config.model)
+        data: Dict = json.loads(response.get("body").read())
+
+        if data.get("error", None) is not None:
+            raise Exception(data.get("error").get("message"))
+
+        return data["outputs"][0]["text"]
 
 
 class AwsBedrockClaudeApi(BaseModel):
@@ -177,7 +262,10 @@ class AwsBedrockClaudeApi(BaseModel):
             region_name=self.region,
         )
 
-    def completion(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+    def chat(self, messages: List[LLMMessage], config: LLMConfig, seed=None):
+        if config.model not in bedrock_claude3_models:
+            raise Exception(f"Must provide a Claude v3 model, got {config.model}")
+
         system_message = [m for m in messages if m.role == "system"]
         user_messages = [m for m in messages if m.role != "system"]
         formated_messages = [m.to_claude_message() for m in user_messages]
@@ -196,9 +284,26 @@ class AwsBedrockClaudeApi(BaseModel):
         if data.get("error", None) is not None:
             raise Exception(data.get("error").get("message"))
 
-        content = data["content"][0]["text"]
+        return data["content"][0]["text"]
 
-        return content
+    def complete(self, prompt: str, config: LLMConfig, seed=None):
+        if config.model not in bedrock_claude2_models:
+            raise Exception(f"Must provide a Claude v2 model, got {config.model}")
+
+        prompt = f"\n\nHuman: {prompt}\n\nAssistant:"
+        data = {
+            "prompt": prompt,
+            "max_tokens_to_sample": config.max_token,
+            "temperature": config.temperature,
+        }
+
+        response = self.bedrock_runtime.invoke_model(body=json.dumps(data), modelId=config.model)
+        data: Dict = json.loads(response.get("body").read())
+
+        if data.get("error", None) is not None:
+            raise Exception(data.get("error").get("message"))
+
+        return data["completion"]
 
 
 LLMApi = Union[OpenAIApi, ClaudeApi]
@@ -252,6 +357,44 @@ class ClaudeApiNode:
         return (ClaudeApi(api_key=claude_api_key, endpoint=endpoint, version=version),)
 
 
+class AwsBedrockMistralApiNode:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "aws_access_key_id": ("STRING", {"multiline": False}),
+                "aws_secret_access_key": ("STRING", {"multiline": False}),
+                "aws_session_token": ("STRING", {"multiline": False}),
+                "region": (aws_regions, {"default": aws_regions[0]}),
+            },
+        }
+
+    RETURN_TYPES = ("LLM_API",)
+    RETURN_NAMES = ("llm_api",)
+    FUNCTION = "create_api"
+    CATEGORY = "ArtVenture/LLM"
+
+    def create_api(self, aws_access_key_id, aws_secret_access_key, aws_session_token, region):
+        if not aws_access_key_id or aws_access_key_id == "":
+            aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID", None)
+        if not aws_secret_access_key or aws_secret_access_key == "":
+            aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
+        if not aws_session_token or aws_session_token == "":
+            aws_session_token = os.environ.get("AWS_SESSION_TOKEN", None)
+
+        if not aws_access_key_id or not aws_secret_access_key:
+            raise Exception("AWS credentials is required.")
+
+        return (
+            AwsBedrockMistralApi(
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                aws_session_token=aws_session_token,
+                region=region,
+            ),
+        )
+
+
 class AwsBedrockClaudeApiNode:
     @classmethod
     def INPUT_TYPES(cls):
@@ -297,7 +440,15 @@ class LLMApiConfigNode:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "model": (gpt_models + claude_models + bedrock_anthropic_models, {"default": gpt_vision_models[0]}),
+                "model": (
+                    gpt_models
+                    + claude3_models
+                    + claude2_models
+                    + bedrock_claude3_models
+                    + bedrock_claude2_models
+                    + bedrock_mistral_models,
+                    {"default": gpt_vision_models[0]},
+                ),
                 "max_token": ("INT", {"default": 1024}),
                 "temperature": ("FLOAT", {"default": 0, "min": 0, "max": 1.0, "step": 0.001}),
             }
@@ -367,7 +518,29 @@ class LLMChatNode:
     CATEGORY = "ArtVenture/LLM"
 
     def chat(self, messages: List[LLMMessage], api: LLMApi, config: LLMConfig, seed):
-        response = api.completion(messages, config, seed)
+        response = api.chat(messages, config, seed)
+        return (response,)
+
+
+class LLMCompletionNode:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True, "dynamicPrompts": False}),
+                "api": ("LLM_API",),
+                "config": ("LLM_CONFIG",),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0x1FFFFFFFFFFFFF}),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("response",)
+    FUNCTION = "chat"
+    CATEGORY = "ArtVenture/LLM"
+
+    def chat(self, prompt: str, api: LLMApi, config: LLMConfig, seed):
+        response = api.complete(prompt, config, seed)
         return (response,)
 
 
@@ -375,16 +548,20 @@ NODE_CLASS_MAPPINGS = {
     "AV_OpenAIApi": OpenAIApiNode,
     "AV_ClaudeApi": ClaudeApiNode,
     "AV_AwsBedrockClaudeApi": AwsBedrockClaudeApiNode,
+    "AV_AwsBedrockMistralApi": AwsBedrockMistralApiNode,
     "AV_LLMApiConfig": LLMApiConfigNode,
     "AV_LLMMessage": LLMMessageNode,
     "AV_LLMChat": LLMChatNode,
+    "AV_LLMCompletion": LLMCompletionNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "AV_OpenAIApi": "OpenAI Api",
-    "AV_ClaudeApi": "Claude Api",
+    "AV_OpenAIApi": "OpenAI API",
+    "AV_ClaudeApi": "Claude API",
     "AV_AwsBedrockClaudeApi": "AWS Bedrock Claude API",
-    "AV_LLMApiConfig": "LLM Api Config",
+    "AV_AwsBedrockMistralApi": "AWS Bedrock Mistral API",
+    "AV_LLMApiConfig": "LLM API Config",
     "AV_LLMMessage": "LLM Message",
     "AV_LLMChat": "LLM Chat",
+    "AV_LLMCompletion": "LLM Completion",
 }
